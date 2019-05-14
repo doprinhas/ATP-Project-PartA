25995e77590336ee803aa97ebcff8cf54e562614
freimans <freimans@post.bgu.ac.il>
2019-05-14 18:52:07 +0300

    ll

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,52 +1,136 @@
 package Server;
 
-import algorithms.search.ASearchingAlgorithm;
-import algorithms.search.BestFirstSearch;
-import algorithms.search.ISearchable;
-import algorithms.search.Solution;
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.Maze;
+import algorithms.search.*;
 
 import java.io.*;
 import java.time.chrono.IsoChronology;
+import java.util.HashMap;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy{
 
-    private ASearchingAlgorithm algorithm;
+    private HashMap<Maze, Integer> mazePool;
+    private int nameCounter;
+    private String mazeTempDirectoryPath;
+    private String solTempDirectoryPath;
+
+    public ServerStrategySolveSearchProblem() {
+        this.mazePool = new HashMap<>();
+        nameCounter = 0;
+
+        String tempDirectoryPath = System.getProperty("java.io.tmpdir");
+        new File(tempDirectoryPath + "\\Maze").mkdir();
+        new File(tempDirectoryPath + "\\Sol").mkdir();
+        mazeTempDirectoryPath = tempDirectoryPath + "\\Maze";
+        solTempDirectoryPath = tempDirectoryPath + "\\Sol";
+    }
 
     @Override
     public void serverStrategy(InputStream inFromClient, OutputStream outToClient){
 
-        ISearchable prob = getProblem(inFromClient);
-        ASearchingAlgorithm algorithm = new BestFirstSearch();
-        Solution solution = algorithm.solve(prob);
-        setSolutionToOutputStream(outToClient , solution);
-
-    }
-
-    private ISearchable getProblem (InputStream stream){
         try {
-            ObjectInputStream fromClient = new ObjectInputStream(stream);
-            ISearchable prob = (ISearchable) fromClient.readObject();
+            // Receiving the problem from client
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            Maze mazeFromClient = (Maze) fromClient.readObject();
+            ISearchable prob = new SearchableMaze(mazeFromClient);
             fromClient.close();
-            return prob;
-        } catch (IOException e){
-            System.out.println("The parameters aren't valid");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e){
-            System.out.println("The sent problem is not valid");
-            e.printStackTrace();
-        }
-        return null;
-    }
 
-    private void setSolutionToOutputStream(OutputStream outToClient , Solution solution){
-        try {
+            Solution solution;
+
+            // Sending to client the problem solution
+            if(!mazePool.containsKey(mazeFromClient))
+            {
+                saveCompMaze(mazeFromClient);
+                ASearchingAlgorithm algorithm = new BestFirstSearch();
+                solution = algorithm.solve(prob);
+                saveSol(solution);
+
+                nameCounter++;
+            }
+            else
+            {
+                solution = retrieveMazeSol(mazeFromClient);
+            }
+
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
             toClient.writeObject(solution);
+
             toClient.flush();
             toClient.close();
-        } catch (IOException e){
-            System.out.println("The parameters aren't valid");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Invalid parametes");
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException | ClassCastException e)
+        {
+            System.out.println("The sent problem is invalid");
             e.printStackTrace();
         }
     }
+
+    private void saveCompMaze(Maze maze)
+    {
+        try {
+            File newMaze = new File(mazeTempDirectoryPath + "\\" + nameCounter);
+            newMaze.createNewFile();
+
+            FileOutputStream out = new FileOutputStream(mazeTempDirectoryPath + "\\" + nameCounter);
+            MyCompressorOutputStream compressor = new MyCompressorOutputStream(out);
+            compressor.write(maze.toByteArray());
+
+            mazePool.put(maze, nameCounter);
+
+            compressor.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Invalid path");
+            e.printStackTrace();
+        }
+    }
+
+    private void saveSol(Solution sol)
+    {
+        try
+        {
+            File newSol = new File(solTempDirectoryPath + "\\" + nameCounter);
+            newSol.createNewFile();
+
+            FileOutputStream out = new FileOutputStream(solTempDirectoryPath + "\\" + nameCounter);
+            ObjectOutputStream objectOut = new ObjectOutputStream(out);
+            objectOut.writeObject(sol);
+
+            objectOut.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Invalid path");
+            e.printStackTrace();
+        }
+    }
+
+    private Solution retrieveMazeSol(Maze maze)
+    {
+        try
+        {
+            FileInputStream in = new FileInputStream(solTempDirectoryPath + "\\" + mazePool.get(maze));
+            ObjectInputStream objectIn = new ObjectInputStream(in);
+            Solution returnSol = (Solution) objectIn.readObject();
+            
+            objectIn.close();
+            
+            return returnSol;
+        }
+        
+        catch(IOException | ClassNotFoundException | ClassCastException e)
+        {
+            System.out.println("Invalid path");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- src/test/test.java
+++ src/test/test.java
@@ -4,7 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         {
-            Short a = new Short((short)4);
+            Integer a = new Integer(256);
             System.out.println(a.byteValue());
         }
     }
